043d7a03668cd931589af45d64b78a697a355c4a
Eetu Mikkonen <mikkoseetu@gmail.com>
2024-02-14 20:27:31 +0200

    viikko5 tehty

---
+++ teht5/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "kerros.h"
+#include "katutaso.h"
+#include "kerrostalo.h"
+
+using namespace std;
+
+int main()
+{
+    kerrostalo k_talo;
+    cout << "Maaritetaan kerrostalon kaikkien kerrosten asunnot" << endl;
+    k_talo.kerros1->maaritaAsunnot();
+    k_talo.kerros2->maaritaAsunnot();
+    k_talo.kerros3->maaritaAsunnot();
+    k_talo.laskeKulutus(1);
+
+    return 0;
+}
